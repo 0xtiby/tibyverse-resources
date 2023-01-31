ea62ea40d1f354c923f2c5f1098be9e2f314bf30
tiby <oxtibyx@gmail.com>
2023-01-31 07:09:02 +0100

    01 - Connect a wallet to react app

---
+++ 01-connect-wallet-to-react-app/pages/_app.tsx
@@ -0,0 +1,39 @@
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import { arbitrum, mainnet, optimism, polygon } from "wagmi/chains";
+
+import type { AppProps } from "next/app";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Connect Wallet App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
